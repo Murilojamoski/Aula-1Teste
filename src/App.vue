c775998008c03e1ca45e9415f102dcb9e2ee494b
MuriloJamoski <mugames2002@gmail.com>
2025-03-18 09:30:16 -0300

    vue clicker 2

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,77 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+const valor = ref(10)
+let click =  ref(1)
+let custo = ref(10)
+let auto = ref(false)
+
+function autaDes(){
+  if(valor.value >= 100){
+    valor.value -= 100
+     auto.value = true
+     console.log(auto.value)
+  }
+  if(auto.value == true){
+  setInterval(()=>{
+    valor.value += 3
+    console.log("+3")
+  }, 1000)
+}
+ 
+}
+
+function incrementar(){
+  valor.value += click.value
+}
+
+function tirar(){
+  valor.value -= click.value
+}
+
+function upgrade(){
+  if (valor.value >= custo.value && click.value < 2){
+    click.value += 1
+    valor.value -= 10
+    custo.value = 100
+    return
+  }
+  if(custo.value >= 100 && custo.value <= 999){
+      click.value += 10
+      valor.value -= 100
+      if(click.value >= 50){
+        custo.value = 1000
+      }
+  }
+  if(valor.value >= custo.value){
+    click.value += 100
+    valor.value -= 1000
+  }
+
+}
+console.log(valor)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <div>
+  <h1>{{ valor }}</h1>
+  <button @click="incrementar()">incrementar</button>
+  <button @click="tirar()">descrementar</button>
+  <button @click="upgrade()">upgrade (custo {{ custo }})</button>
+  <button @click="autaDes()">autoCliker (custo 100)</button>
+  <button @click="dbg">Gerente</button>
+  <p>valor do clique: {{ click }}</p>
 
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  button{
+    margin: 1vw;
+    padding: 5px;
+    border-radius: 10px;
+    border: none;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  div{
+    text-align: center;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
